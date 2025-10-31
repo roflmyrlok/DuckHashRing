108d6fc2cfb45d7b7f8e39a6796dbc424d7dd8b4
Andrii <atrybushnyi@kse.org.ua>
2025-10-31 20:04:27 +0200

    Simple db, k8s 3 copy

--- DuckSharding.Coordinator/Program.cs
+++ DuckSharding.Coordinator/Program.cs
@@ -1,4 +1,5 @@
 using DuckSharding.Coordinator;
+using DuckSharding.Shared.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +13,22 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<Coordinator>();
 builder.Services.AddSingleton<ShardClient>();
-builder.Services.AddSingleton<ShardProcessManager>();
 
 var app = builder.Build();
+
+var coordinator = app.Services.GetRequiredService<Coordinator>();
+var configuration = app.Configuration;
+var k8sNamespace = configuration["Kubernetes:Namespace"] ?? "default";
+
+for (int i = 1; i <= 3; i++)
+{
+	var shardId = $"shard-{i}";
+	var shardInfo = new ShardInfo(
+		shardId, 
+		$"{shardId}.{k8sNamespace}.svc.cluster.local", 
+		8080);
+	coordinator.AddShard(shardInfo);
+}
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
